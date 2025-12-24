905a70f7016a425afdb0a4228e7ec6d22db84bac
rafaalvesf <rafaalvesf19@gmail.com>
2025-12-23 21:50:26 -0300

    Versao Bruta para testar casos

--- src/main/java/com/nubank/nubank/service/TransacaoService.java
+++ src/main/java/com/nubank/nubank/service/TransacaoService.java
@@ -6,8 +6,8 @@ import com.nubank.nubank.dto.TransacaoDto;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
-
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,16 +17,68 @@ public class TransacaoService {
     public List<TaxDto> executarTransacao(List<TransacaoDto> transacoes) {
 
         List<TaxDto> tax = new ArrayList<>();
-        BigDecimal mediaPonderada;
+        BigDecimal mediaPonderada = BigDecimal.ZERO;
+        int quantidadeDeAcoesAtual = 0;
+        BigDecimal prejuizoAcumulado = BigDecimal.ZERO;
+        final BigDecimal VALOR_LIVRE_IMPOSTO = BigDecimal.valueOf(20000.00);
+        final BigDecimal TAXA_IMPOSTO = new BigDecimal("0.20");
 
         for(TransacaoDto transacao : transacoes){
+            TaxDto taxDto = new TaxDto();
 
-            transacao.getOperation();
+            if ("buy".equals(transacao.getOperation())) {
 
-            TaxDto taxDto = new TaxDto(BigDecimal.ZERO);
+                BigDecimal custoAtual = mediaPonderada.multiply(BigDecimal.valueOf(quantidadeDeAcoesAtual));
+                BigDecimal custoNovaCompra = transacao.getUnitCost().multiply(BigDecimal.valueOf(transacao.getQuantity()));
+                int novaQuantidade = quantidadeDeAcoesAtual + transacao.getQuantity();
+
+                mediaPonderada = custoAtual.add(custoNovaCompra).divide(BigDecimal.valueOf(novaQuantidade),2,RoundingMode.HALF_UP);
+
+                quantidadeDeAcoesAtual = novaQuantidade;
+
+                taxDto.setTax(BigDecimal.ZERO);
+            }
+            else{
+
+                BigDecimal lucroTributavel = (transacao.getUnitCost().subtract(mediaPonderada)).multiply(BigDecimal.valueOf(transacao.getQuantity()));
+                BigDecimal valorDaOperacao = transacao.getUnitCost().multiply(BigDecimal.valueOf(transacao.getQuantity()));
+                quantidadeDeAcoesAtual = quantidadeDeAcoesAtual - transacao.getQuantity();
+
+                if (valorDaOperacao.compareTo(VALOR_LIVRE_IMPOSTO) > 0) {
+
+                    if (lucroTributavel.compareTo(BigDecimal.ZERO) > 0 &&
+                            prejuizoAcumulado.compareTo(BigDecimal.ZERO) > 0) {
+
+                        if (lucroTributavel.compareTo(prejuizoAcumulado) >= 0) {
+                            lucroTributavel = lucroTributavel.subtract(prejuizoAcumulado);
+                            prejuizoAcumulado = BigDecimal.ZERO;
+                        } else {
+                            prejuizoAcumulado = prejuizoAcumulado.subtract(lucroTributavel);
+                            lucroTributavel = BigDecimal.ZERO;
+                        }
+                    }
+
+                    if (lucroTributavel.compareTo(BigDecimal.ZERO) > 0) {
+                        taxDto.setTax(lucroTributavel.multiply(TAXA_IMPOSTO)
+                                .setScale(2, RoundingMode.HALF_UP));
+                    } else {
+                        if (lucroTributavel.compareTo(BigDecimal.ZERO) < 0) {
+                            prejuizoAcumulado = prejuizoAcumulado.add(lucroTributavel.abs());
+                        }
+                        taxDto.setTax(BigDecimal.ZERO);
+                    }
+
+                }
+                else {
+                    if (lucroTributavel.compareTo(BigDecimal.ZERO) < 0) {
+                        prejuizoAcumulado = prejuizoAcumulado.add(lucroTributavel.abs());
+                    }
+                    taxDto.setTax(BigDecimal.ZERO);
+                }
+            }
+
             tax.add(taxDto);
         }
-
         return tax;
     }
 }
